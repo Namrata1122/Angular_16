302d787cf89be88541a417f4b86bf0db8ab8b337
Namrata Kumari <namrata.kumari@argusoft.com>
2025-09-03 18:45:01 +0530

    Subject

--- angular_observables/src/app/Services/task.service.ts
+++ angular_observables/src/app/Services/task.service.ts
@@ -1,14 +1,18 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable({
     providedIn:'root'
 })
 export class TaskService{
     //creating an event
-    createTask:EventEmitter<string> = new EventEmitter<string>();
+    // createTask:EventEmitter<string> = new EventEmitter<string>();
+
+    // using subject
+    createTask = new Subject<string>();
 
     // raising an event when something happens
     OnCreateTask(value:string){
-        this.createTask.emit(value);
+        this.createTask.next(value);
     }
 }
