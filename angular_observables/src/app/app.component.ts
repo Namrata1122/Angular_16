1a6d24352ae1001f45302a66bd2eacdae7712c99
Namrata Kumari <namrata.kumari@argusoft.com>
2025-09-03 14:44:47 +0530

    error and complete methods in observables

--- angular_observables/src/app/app.component.ts
+++ angular_observables/src/app/app.component.ts
@@ -18,8 +18,13 @@ export class AppComponent {
     setTimeout(()=>observer.next(1),1000);
     setTimeout(()=>observer.next(2),2000);
     setTimeout(()=>observer.next(3),3000);
+    // Emitting an error
+    setTimeout(()=>observer.error(new Error('Something went wrong. please try again later!')),3000);
+    // data after emitting error is not emitted
     setTimeout(()=>observer.next(4),4000);
     setTimeout(()=>observer.next(5),5000);
+    // after complete signal is sent no more data can be emitted
+    setTimeout(()=>observer.complete(),3000);
   });
 
   
@@ -30,6 +35,14 @@ export class AppComponent {
     // - next,error,complete
     this.myObservable.subscribe((val:any)=>{
       this.data.push(val);
-    });
+    },
+    // handling error event
+    (err)=>{
+      alert(err.message);
+    },
+    ()=>{
+      alert('All data is streamed');
+    }
+  );
   }
 }
