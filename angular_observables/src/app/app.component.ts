c18a33147178db7e435d66999d36bea084bf6cbc
Namrata Kumari <namrata.kumari@argusoft.com>
2025-09-03 14:21:23 +0530

    creating and using an observable

---
+++ angular_observables/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular_observables';
+
+  data: any[]=[];
+
+  //1.create an observable
+  
+  //observable-event emitter
+  myObservable = new Observable((observer)=>{
+    setTimeout(()=>observer.next(1),1000);
+    setTimeout(()=>observer.next(2),2000);
+    setTimeout(()=>observer.next(3),3000);
+    setTimeout(()=>observer.next(4),4000);
+    setTimeout(()=>observer.next(5),5000);
+  });
+
+  
+  GetAsyncData(){
+
+    //Observer-event listener
+    //subscribe method takes three optional callback functions
+    // - next,error,complete
+    this.myObservable.subscribe((val:any)=>{
+      this.data.push(val);
+    });
+  }
+}
